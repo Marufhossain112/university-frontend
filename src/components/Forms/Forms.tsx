cbce47ebb25488a8a658648791abef28322cf1fb
maruf <dev.maruf112@gmail.com>
2023-09-29 21:07:20 +0600

    49-6_Create reusable Form using FormProvider of React Hook from

---
+++ src/components/Forms/Forms.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement, ReactNode } from 'react';
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+
+type FormConfig = {
+    defaultValues?: Record<string, any>;
+};
+
+type FormProps = {
+    children?: ReactElement | ReactNode;
+    submitHandler: SubmitHandler<any>;
+} & FormConfig;
+export default function Form({ children, submitHandler, defaultValues }: FormProps) {
+    const FormConfig: FormConfig = {};
+    if (!!defaultValues) FormConfig["defaultValues"] = defaultValues;
+    const methods = useForm<FormProps>(FormConfig);
+    const { handleSubmit, reset } = methods;
+    const onSubmit = (data: any) => { handleSubmit(data), reset(); };
+    return (
+        <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {children}
+            </form>
+        </FormProvider>
+    );
+}
