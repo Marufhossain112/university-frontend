986483a46851fcf293576e5c464eb0e3d2082219
maruf <dev.maruf112@gmail.com>
2023-09-30 09:52:18 +0600

    fixed issues

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,92 +20,47 @@ export default function LoginPage() {
 
     };
     return (
-        <Row>
-            <Col sm={12} md={16} lg={18}>
-                <Image src={loginImage} width={500} alt='Login image'></Image>
+        <Row
+            justify="center"
+            align="middle"
+            style={{
+                minHeight: "100vh",
+            }}
+        >
+            <Col sm={12} md={16} lg={10}>
+                <Image src={loginImage} width={500} alt="login image" />
             </Col>
             <Col sm={12} md={8} lg={8}>
-                <h1>First login your account</h1>
+                <h1
+                    style={{
+                        margin: "15px 0px",
+                    }}
+                >
+                    First login your account
+                </h1>
                 <div>
                     <Form submitHandler={onSubmit}>
                         <div>
-                            <FormInput name='id' type='text' size='large' label='user id' />
+                            <FormInput name="id" type="text" size="large" label="User Id" />
                         </div>
-                        <div>
-                            <FormInput name='password' type='password' size='large' label='password' />
+                        <div
+                            style={{
+                                margin: "15px 0px",
+                            }}
+                        >
+                            <FormInput
+                                name="password"
+                                type="password"
+                                size="large"
+                                label="User Password"
+                            />
                         </div>
-                        <Button type='primary' htmlType='submit'>Login</Button>
+                        <Button type="primary" htmlType="submit">
+                            Login
+                        </Button>
                     </Form>
                 </div>
             </Col>
-        </Row >
+        </Row>
     );
 }
-
-
-// "use client";
-// import { Button, Col, Input, Row } from "antd";
-// import loginImage from "../../assets/Login-bro.svg";
-// import Image from "next/image";
-// import FormInput from "@/components/Forms/FormInput";
-// import { SubmitHandler } from "react-hook-form";
-// import Form from "@/components/Forms/Forms";
-
-// type FormValues = {
-//     id: string;
-//     password: string;
-// };
-
-// const LoginPage = () => {
-//     const onSubmit: SubmitHandler<FormValues> = (data) => {
-//         try {
-//             console.log(data);
-//         } catch (err) { }
-//     };
-//     return (
-//         <Row
-//             justify="center"
-//             align="middle"
-//             style={{
-//                 minHeight: "100vh",
-//             }}
-//         >
-//             <Col sm={12} md={16} lg={10}>
-//                 <Image src={loginImage} width={500} alt="login image" />
-//             </Col>
-//             <Col sm={12} md={8} lg={8}>
-//                 <h1
-//                     style={{
-//                         margin: "15px 0px",
-//                     }}
-//                 >
-//                     First login your account
-//                 </h1>
-//                 <div>
-//                     <Form submitHandler={onSubmit}>
-//                         <div>
-//                             <FormInput name="id" type="text" size="large" label="User Id" />
-//                         </div>
-//                         <div
-//                             style={{
-//                                 margin: "15px 0px",
-//                             }}
-//                         >
-//                             <FormInput
-//                                 name="password"
-//                                 type="password"
-//                                 size="large"
-//                                 label="User Password"
-//                             />
-//                         </div>
-//                         <Button type="primary" htmlType="submit">
-//                             Login
-//                         </Button>
-//                     </Form>
-//                 </div>
-//             </Col>
-//         </Row>
-//     );
-// };
-
-// export default LoginPage;
