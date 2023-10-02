7fd4c4c5a7bf94b8aa79acf7331c4ceb30d76ee5
maruf <dev.maruf112@gmail.com>
2023-10-02 14:06:17 +0600

    title

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,76 +1,11 @@
-"use client";
+import LoginPage from '@/components/login/login';
+import { Metadata } from 'next';
 import React from 'react';
-import { Button, Col, Row } from 'antd';
-import loginImage from "../../assets/Login-bro.svg";
-import Image from 'next/image';
-import FormInput from '@/components/Forms/FormInput';
-import Form from '@/components/Forms/Forms';
-import { SubmitHandler } from 'react-hook-form';
-import { useUserLoginMutation } from '../redux/api/authApi';
-import { storeUserInfo } from '@/services/auth.service';
-import { useRouter } from 'next/navigation';
-type FormValues = {
-    id: string,
-    password: string;
+export const metadata: Metadata = {
+    title: "UMS | Login"
 };
-export default function LoginPage() {
-    const router = useRouter();
-    // console.log(getUserInfo());
-    const [userLogin] = useUserLoginMutation();
-    // console.log(isLoggedIn());
-    const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
-        try {
-            const res = await userLogin({ ...data }).unwrap();
-            if (res?.accessToken) {
-                router.push("/profile");
-            }
-            storeUserInfo({ accessToken: res?.accessToken });
-        } catch (error: any) {
-            console.log(error.message);
-        }
-    };
+export default function Login() {
     return (
-        <Row
-            justify="center"
-            align="middle"
-            style={{
-                minHeight: "100vh",
-            }}
-        >
-            <Col sm={12} md={16} lg={10}>
-                <Image src={loginImage} width={500} alt="login image" />
-            </Col>
-            <Col sm={12} md={8} lg={8}>
-                <h1
-                    style={{
-                        margin: "15px 0px",
-                    }}
-                >
-                    First login your account
-                </h1>
-                <div>
-                    <Form submitHandler={onSubmit}>
-                        <div>
-                            <FormInput name="id" type="text" size="large" label="User Id" />
-                        </div>
-                        <div
-                            style={{
-                                margin: "15px 0px",
-                            }}
-                        >
-                            <FormInput
-                                name="password"
-                                type="password"
-                                size="large"
-                                label="User Password"
-                            />
-                        </div>
-                        <Button type="primary" htmlType="submit">
-                            Login
-                        </Button>
-                    </Form>
-                </div>
-            </Col>
-        </Row>
+        <LoginPage></LoginPage>
     );
 }
---
+++ src/components/login/login.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from 'react';
+import { Button, Col, Row } from 'antd';
+import loginImage from "../../assets/Login-bro.svg";
+import Image from 'next/image';
+import FormInput from '@/components/Forms/FormInput';
+import Form from '@/components/Forms/Forms';
+import { SubmitHandler } from 'react-hook-form';
+import { storeUserInfo } from '@/services/auth.service';
+import { useRouter } from 'next/navigation';
+import { useUserLoginMutation } from '@/app/redux/api/authApi';
+type FormValues = {
+    id: string,
+    password: string;
+};
+export default function LoginPage() {
+    const router = useRouter();
+    // console.log(getUserInfo());
+    const [userLogin] = useUserLoginMutation();
+    // console.log(isLoggedIn());
+    const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
+        try {
+            const res = await userLogin({ ...data }).unwrap();
+            if (res?.accessToken) {
+                router.push("/profile");
+            }
+            storeUserInfo({ accessToken: res?.accessToken });
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    };
+    return (
+        <Row
+            justify="center"
+            align="middle"
+            style={{
+                minHeight: "100vh",
+            }}
+        >
+            <Col sm={12} md={16} lg={10}>
+                <Image src={loginImage} width={500} alt="login image" />
+            </Col>
+            <Col sm={12} md={8} lg={8}>
+                <h1
+                    style={{
+                        margin: "15px 0px",
+                    }}
+                >
+                    First login your account
+                </h1>
+                <div>
+                    <Form submitHandler={onSubmit}>
+                        <div>
+                            <FormInput name="id" type="text" size="large" label="User Id" />
+                        </div>
+                        <div
+                            style={{
+                                margin: "15px 0px",
+                            }}
+                        >
+                            <FormInput
+                                name="password"
+                                type="password"
+                                size="large"
+                                label="User Password"
+                            />
+                        </div>
+                        <Button type="primary" htmlType="submit">
+                            Login
+                        </Button>
+                    </Form>
+                </div>
+            </Col>
+        </Row>
+    );
+}
